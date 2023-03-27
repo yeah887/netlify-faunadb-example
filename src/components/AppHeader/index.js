d2c944cb6f55d9fb01e104413311a7675b6fde5e
Chris <christian.kretzer1@gmail.com>
2023-03-27 13:52:51 +0200

    ApperHEader

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -12,9 +12,12 @@ const AppHeader = (props) => {
           <div className='app-left-nav'>
             <img src={logo} className='app-logo' alt='logo' />
             <div className='app-title-text'>
-              <h1 className='app-title'>Netlify + Fauna DB</h1>
+              <h1 className='app-title'>Netlify + Fauna DB ++++++</h1>
               <p className='app-intro'>
                 Using FaunaDB & Netlify functions
+              </p>
+              <p className='app-title-wrapper'>
+                Download to...
               </p>
             </div>
           </div>
